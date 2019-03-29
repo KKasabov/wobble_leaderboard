d935aa5c4e3abb29c7c61c5e7a6f82adc4c64e69
Ivan Kasabov <ivan@midiaresearch.com>
2019-03-29 02:17:07 +0000

    Cleaned up code and added comments

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -2,10 +2,11 @@ import React from 'react';
 import firebase from './Firebase';
 
 class Leaderboard extends React.Component {
+    //users list
     state = { Users: [] };
 
     componentDidMount() {
-        var that = this;
+        //populate a list of users with the data from firebase
         firebase
             .firestore()
             .collection('leaderboard')
@@ -19,16 +20,19 @@ class Leaderboard extends React.Component {
                         time: doc.data().time
                     });
                 });
+                //update the state
                 this.setState({ Users });
             });
     }
 
+    //render a list of users and their times
     render() {
         return (
             <ol>
                 {this.state.Users.map(u => {
                     return (
                         <li key={u.id}>
+                        {/* ensure the text in each row is aligned both left and right */}
                             <h4>{u.firstname} <span className='fR'>{u.time} s</span></h4>
                         </li>
                     );
